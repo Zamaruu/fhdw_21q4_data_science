b46590f59487523c54c159916f73219749096b9d
Maximilian Ditz <max.ditz@outlook.de>
2021-11-04 19:58:47 +0100

    mögliche flächen hinzugefügt

--- webpage/js/initFabricCore.js
+++ webpage/js/initFabricCore.js
@@ -4,3 +4,31 @@ var ButtonElements = document.querySelectorAll(".ms-Button");
         // Insert Event Here
     });
 }
+
+var PanelExamples = document.getElementsByClassName("ms-PanelExample");
+  for (var i = 0; i < PanelExamples.length; i++) {
+    (function() {
+      var PanelExampleButton = PanelExamples[i].querySelector(".ms-Button");
+      var PanelExamplePanel = PanelExamples[i].querySelector(".ms-Panel");
+      PanelExampleButton.addEventListener("click", function(i) {
+        new fabric['Panel'](PanelExamplePanel);
+      });
+    }());
+  }
+
+var CalloutExamples = document.querySelectorAll(".ms-CalloutExample");
+  for (var i = 0; i < CalloutExamples.length; i++) {
+    var Example = CalloutExamples[i];
+    var ExampleButtonElement = Example.querySelector(".ms-CalloutExample-button .ms-Button");
+    var CalloutElement = Example.querySelector(".ms-Callout");
+    new fabric['Callout'](
+      CalloutElement,
+      ExampleButtonElement,
+      "right"
+    );
+  }
+
+var SpinnerElements = document.querySelectorAll(".ms-Spinner");
+  for (var i = 0; i < SpinnerElements.length; i++) {
+    new fabric['Spinner'](SpinnerElements[i]);
+  }
--- webpage/js/navbar.js
+++ webpage/js/navbar.js
@@ -1,8 +1,11 @@
 function navbarClick() {
     var x = document.getElementById("myTopnav");
+    var icon = document.getElementById("topnav-icon")
     if (x.className === "topnav") {
         x.className += " responsive";
+        icon.className = "ms-Icon ms-Icon--Cancel";
     } else {
         x.className = "topnav";
+        icon.className = "ms-Icon ms-Icon--GlobalNavButton";
     }
 }
